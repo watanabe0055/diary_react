d6a59763fe85ee69506c1047f68ee4971e230e4a
watanabe0055 <mailok1212+github@gmail.com>
2022-12-12 02:01:27 +0900

    400ページの作成とルーティング

---
+++ src/pages/error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <>
+      <div>
+        <p>400エラー</p>
+        <p>このページは閲覧できません！！</p>
+      </div>
+    </>
+  );
+}
