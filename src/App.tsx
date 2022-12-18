38ff3ee347642b914f8eef0a33ed23f5edf5009a
watanabe0055 <mailok1212+github@gmail.com>
2022-12-18 14:24:11 +0900

    日記投稿機能のAPI繋ぎ込み

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./atom/header";
 import Login from "./pages/login/login";
 import DiaryIndex from "./pages/diary/diaryIndex";
+import DiaryRegster from "./pages/diary/diaryRegister";
 import DiaryDetail from "./pages/diary/diaryDetail";
 import DiaryEdit from "./pages/diary/diaryEdit";
 import Error from "./pages/error";
@@ -28,6 +29,7 @@ function App() {
           <Routes>
             <Route path={`/login/`} element={<Login />} />
             <Route path={`/diary/`} element={<DiaryIndex />} />
+            <Route path={`/diary/register`} element={<DiaryRegster />} />
             <Route path={`/diary/:id`} element={<DiaryDetail />} />
             <Route path={`/diary/:id/update`} element={<DiaryEdit />} />
             <Route path={`Error`} element={<Error />} />
---
+++ src/pages/diary/diaryRegister.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import axios from "axios";
+import Cookies from "js-cookie";
+
+//マテリアル UI
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
+
+const TextFiledBlock = styled.div`
+  margin-top: 20px;
+`;
+
+export default function DiaryRegister() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const generalApiInterface = axios.create({
+    baseURL: `http://localhost:3000/api/v1/diary/`,
+    headers: {
+      uid: Cookies.get("uid"),
+      client: Cookies.get("client"),
+      access_token: Cookies.get("access-token"),
+    },
+  });
+
+  function UseFeathDiaryCreate() {
+    console.log("test");
+    generalApiInterface
+      .post(`http://localhost:3000/api/v1/diary/`, {
+        title: title,
+        content: content,
+        emotion_id: "2",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function (error) {
+        console.log(error.response.data);
+      });
+  }
+
+  const handleOnCreatetTitle = (title: string) => {
+    setTitle(title);
+  };
+
+  const handleOnCreatetContent = (content: string) => {
+    setContent(content);
+  };
+
+  const SuccsesElm = (
+    <>
+      <div>
+        <TextFiledBlock>
+          <TextField
+            id="outlined-multiline-static"
+            label="タイトル"
+            multiline
+            rows={2}
+            value={title}
+            onChange={(e) => handleOnCreatetTitle(e.target.value)}
+            sx={{
+              padding: "12px",
+              "@media screen and (max-width:480px)": {
+                width: "340px",
+              },
+              "@media screen and (min-width:768px)": {
+                width: "720px",
+              },
+              "@media screen and (min-width:990px)": {
+                width: "1300px",
+              },
+            }}
+          />
+        </TextFiledBlock>
+        <TextFiledBlock>
+          <TextField
+            id="filled-multiline-static"
+            label="コンテンツ"
+            multiline
+            rows={8}
+            value={content}
+            onChange={(e) => handleOnCreatetContent(e.target.value)}
+            sx={{
+              padding: "12px",
+              "@media screen and (max-width:480px)": {
+                width: "340px",
+              },
+              "@media screen and (min-width:768px)": {
+                width: "720px",
+              },
+              "@media screen and (min-width:990px)": {
+                width: "1300px",
+              },
+            }}
+          />
+        </TextFiledBlock>
+        <Button
+          onClick={() => UseFeathDiaryCreate()}
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          送信
+        </Button>
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <div>新規作成画面</div>
+      <div>{SuccsesElm}</div>
+    </>
+  );
+}
